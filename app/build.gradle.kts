33624fbb637885abeaf6533b3b0719bd553424aa
harshmali <sainiharsh754@gmail.com>
2024-06-02 00:23:52 +0530

    initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,73 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.google.gms.google.services)
+    alias(libs.plugins.google.firebase.crashlytics)
+}
+
+android {
+    namespace = "com.app.boltfax"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.app.boltfax"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    // FIREBASE COMPONENT
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
+    implementation(libs.firebase.crashlytics)
+
+    // NAVIGATION COMPONENT
+    implementation(libs.androidx.navigation)
+    implementation(libs.androidx.navigation.ui)
+
+    // RETROFIT
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.gson)
+
+    // OKHTTP 3 INTERCEPTOR
+    implementation(libs.okhttp3.interceptor)
+
+    // DATA STORE
+    implementation(libs.datastore)
+
+    // COUNTRY CODE PICKER
+    implementation(libs.countryCodePicker)
+
+}
